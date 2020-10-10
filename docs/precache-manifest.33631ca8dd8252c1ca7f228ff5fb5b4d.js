ea807696efe2eed5ff4e321efba01987d42450b7
RodPraJav <javier@javier.com>
2020-10-10 12:12:44 +0200

    docs agregado

---
+++ docs/precache-manifest.33631ca8dd8252c1ca7f228ff5fb5b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0da9b3cd65d41a10232ad6c84c69f81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982f46e399e54242a5f0",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "fea7e2e7751d2afa911c",
+    "url": "/static/css/main.3313bb5d.chunk.css"
+  },
+  {
+    "revision": "982f46e399e54242a5f0",
+    "url": "/static/js/2.d4a7ae16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d4a7ae16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea7e2e7751d2afa911c",
+    "url": "/static/js/main.d4b8159e.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
